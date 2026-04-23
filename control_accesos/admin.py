7de0510e7ec298f737808c53c86a8a7ea1feca55
Chej <chej.sidi.53388@ikasle.egibide.org>
2026-04-23 21:01:06 +0200

    Fix: Registramos los modulos creados

--- control_accesos/admin.py
+++ control_accesos/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Usuario, Zona, Permiso, RegistroAcceso
 
-# Register your models here.
+
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ['dni', 'apellidos', 'nombre', 'email', 'tipo']
+    search_fields = ['dni', 'nombre', 'apellidos', 'email']
+    list_filter = ['tipo']
+
+
+@admin.register(Zona)
+class ZonaAdmin(admin.ModelAdmin):
+    list_display = ['codigo', 'nombre', 'nivel_acceso', 'ubicacion']
+    list_filter = ['nivel_acceso']
+    search_fields = ['codigo', 'nombre']
+
+
+@admin.register(Permiso)
+class PermisoAdmin(admin.ModelAdmin):
+    list_display = ['codigo', 'usuario', 'zona', 'fecha_inicio', 'fecha_fin', 'estado']
+    list_filter = ['estado']
+    search_fields = ['codigo']
+
+
+@admin.register(RegistroAcceso)
+class RegistroAccesoAdmin(admin.ModelAdmin):
+    list_display = ['codigo', 'usuario', 'zona', 'fecha', 'hora', 'resultado']
+    list_filter = ['resultado', 'fecha']
+    search_fields = ['codigo']
