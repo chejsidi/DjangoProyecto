51cbc9f843ecfa3d68112493639948fa76d28fce
Chej <chej.sidi.53388@ikasle.egibide.org>
2026-05-27 19:40:58 +0200

    Feat: Variables de entorno Feat: Base de datos postgresql en neon

--- egibide_access/settings.py
+++ egibide_access/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import dj_database_url
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +31,9 @@ SECRET_KEY = 'django-insecure-kilx7&=cymra4eo*okiopbhz1vy4ga28&f9z@3=ndz+60_$de*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.ngrok-free.app']
+
+CSRF_TRUSTED_ORIGINS = ['https://*.ngrok-free.app']
 
 
 # Application definition
@@ -91,10 +98,11 @@ WSGI_APPLICATION = 'egibide_access.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 
